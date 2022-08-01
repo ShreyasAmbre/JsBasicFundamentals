d3019fd21549daca9d7dec102639cbbe5c1e8f47
ShreyasAmbre <shreyas@rupeeseed.com>
2022-08-01 20:36:58 +0530

    first commit

---
+++ keyword.js
@@ -0,0 +1,39 @@
+// variable, const & let
+
+/*  tools :- variable
+    purpose :- variable overrite karu shakto , parat declared karu shakto, data stored dynamic 
+*/
+/*  tools :- const
+    purpose :-  fixed value asel tevha const keyword use hote , constant variable is not declared and overwrite value. 
+*/
+/*  tools :- let
+    purpose :- let keyword apan brackets madhe user karto mostly, 
+    jheva data confidentail asel kiva tya data cha use apan fakta tya specific SCOPE madhech asel theva use karto 
+    so jene karun unnecc. memory location allot karun rhanar nahi
+*/
+
+// var mondayTemp = "40 Degree Celcius"
+// console.log("Weather Forcaste Morning =>", mondayTemp)
+// mondayTemp = "48 Degree Celcius"
+// console.log("Weather Forcaste Afternoon =>", mondayTemp)
+
+//===========================================================================
+
+// const name = "Shreyas"
+// console.log("MY NAME ==>", name)
+// name = "Shreya"
+// console.log("MY NAME 2", name)
+
+//===========================================================================
+
+
+if(true){
+    let raius = 10
+    console.log("BANK ACC No. =>", bankAccNo)
+}
+
+// Practice 
+/*
+    What is variable declaration and Initialization ? 
+*/
+
---
+++ printMsg.js
@@ -0,0 +1,24 @@
+// Tool :- console.log()
+// purpose :- to print something mention inside brackets, to print what data is present inside variable
+
+
+console.log("Hi Neha"); //msg print
+console.error("Required 2 Numbers")
+
+
+var name = "Neha"
+console.log(name) //variable 
+
+
+var dataApi = name
+console.log("DATA FROM API ==>", dataApi)
+
+// Practice
+// => Print variable inside quotes in console.log() 
+// => Print conditin inside console.log()
+
+
+
+
+
+
